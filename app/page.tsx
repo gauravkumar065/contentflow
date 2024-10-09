959f7af44935b43cf13bc099e4133d90bd53b1f5
gaurav <gauravkmr065@gmail.com>
2024-10-09 11:29:48 +0530

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { AccordionComponent } from "@/components/homepage/accordion-component";
-import BlogSample from "@/components/homepage/blog-samples";
+"use client";
 import HeroSection from "@/components/homepage/hero-section";
-import MarketingCards from "@/components/homepage/marketing-cards";
 import Pricing from "@/components/homepage/pricing";
 import SideBySide from "@/components/homepage/side-by-side";
 import PageWrapper from "@/components/wrapper/page-wrapper";
@@ -16,20 +14,12 @@ export default function Home() {
       <div className="my-[8rem] flex w-full items-center justify-center">
         <SideBySide />
       </div>
-      {/* <div className="flex w-full flex-col items-center justify-center p-2">
-        <MarketingCards />
-      </div> */}
-      {/* <div className="max-w-[1200px] p-8 mt-[2rem] lg:mt-[6rem] lg:mb-[5rem]">
-        <BlogSample />
-      </div> */}
+
       {config.auth.enabled && config.payments.enabled && (
         <div>
           <Pricing />
         </div>
       )}
-      {/* <div className="my-[8rem] flex w-full items-center justify-center">
-        <AccordionComponent />
-      </div> */}
     </PageWrapper>
   );
 }
